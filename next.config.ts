28acf193f64579c0536abee01db56fafe8002b29
freddyFLux <fredrik@digidevs.no>
2026-03-13 11:20:09 +0100

    teams deployment fixes

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "frame-ancestors https://*.teams.microsoft.com https://*.teams.live.com https://*.skype.com https://*.teams.microsoft.us https://*.gov.teams.microsoft.us 'self'",
+              "frame-ancestors https://*.teams.microsoft.com https://teams.cloud.microsoft https://*.teams.cloud.microsoft https://*.teams.live.com https://*.skype.com https://*.teams.microsoft.us https://*.gov.teams.microsoft.us 'self'",
           },
         ],
       },
